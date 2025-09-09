c7ab5b108873c6f8934cd4c769f1aa1cad5863f4
GOETTFERT\rwollenschlaeger <r.wollenschlaeger@goettfert.de>
2025-09-09 16:19:12 +0200

    bme280 read is working

--- ESP-BME.ino
+++ ESP-BME.ino
@@ -13,7 +13,8 @@ forget, can't receive anything.
 
 //define static vars
 #define SEALEVELPRESSURE_HPA (1013.25)
-#define BME_VCC 15
+#define I2C_SDA 12  //Default GPIO21
+#define I2C_SCL 22  //Default GPIO22
 #define SAMPLING_COUNT 10
 #define SAMPLING_DELAY 1000
 #define SLEEP_TIME 900e6
@@ -28,6 +29,7 @@ char measurands[][50] = { "TEMPERATURE", "PRESSURE", "ALTITUDE", "HUMIDTY", "VOL
 // uint8_t MAC_SELF[] = { 0x84, 0xF3, 0xEB, 0x05, 0x43, 0xE8 };
 // uint8_t MAC_RECEIVER[] = {0x84, 0x0D, 0x8E, 0xB7, 0xFE, 0x1B};
 
+TwoWire I2CBME = TwoWire(0);
 Adafruit_BME280 bme;  // I2C --> I2C only available on GPIO 4+5
 int dpID = 0;
 int rainSensorCounter = 0;
@@ -36,14 +38,14 @@ int rainSensorCounter = 0;
 * Setup function
 */
 void setup() {
-  // put your setup code here, to run once:  
+  // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(BME_VCC, OUTPUT);
-  digitalWrite(BME_VCC, HIGH);
-  digitalWrite(LED_BUILTIN, LOW);  
+  digitalWrite(LED_BUILTIN, LOW);
+
+  I2CBME.begin(I2C_SDA, I2C_SCL, 100000);
 
   //Interrupt for Windspeed
-  // attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), rainSensorInterrupt, FALLING);  
+  // attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), rainSensorInterrupt, FALLING);
 
   //Important: Start Serial com after activating outputs
   Serial.begin(115200);
@@ -63,8 +65,8 @@ void setup() {
   // esp_now_add_peer(MAC_RECEIVER, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 
   //Initialize BME
-  bool status; 
-  status = bme.begin(0x76);
+  bool status;
+  status = bme.begin(0x76, &I2CBME);
   if (!status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     exit(1);
@@ -74,33 +76,33 @@ void setup() {
 /**
 * Loop function
 */
-void loop() {  
+void loop() {
   //calc meanVals
   bme280Struct meanVals;
-  sampleData(&meanVals);     
+  sampleData(&meanVals);
 
   //Send all meanVals per ESPNow
-  for(int i = 0; i< 4; i++){
+  for (int i = 0; i < 4; i++) {
     comStruct send;
     send.datapointID = dpID++;
-    sprintf(send.id, "%lu", bme.sensorID());        
+    sprintf(send.id, "%lu", bme.sensorID());
     strcpy(send.sensorType, "bme280");
     strcpy(send.key, measurands[i]);
-    switch(i){
-      case 0:        
-        send.dValue = meanVals.t;        
+    switch (i) {
+      case 0:
+        send.dValue = meanVals.t;
         break;
       case 1:
-        send.dValue = meanVals.p;        
+        send.dValue = meanVals.p;
         break;
       case 2:
-        send.dValue = meanVals.alt;        
+        send.dValue = meanVals.alt;
         break;
       case 3:
-        send.dValue = meanVals.hum;        
+        send.dValue = meanVals.hum;
         break;
     }
-    
+
     //fill with empty string
     strcpy(send.sValue, "");
     //Send to Receiver
@@ -112,22 +114,22 @@ void loop() {
   rainAmount.datapointID = dpID++;
   strcpy(rainAmount.id, "ane");
   strcpy(rainAmount.sensorType, "anemometer");
-  strcpy(rainAmount.key, measurands[5]);  
+  strcpy(rainAmount.key, measurands[5]);
   strcpy(rainAmount.sValue, "");
   rainAmount.dValue = rainSensorCounter * 450;
 
   rainSensorCounter = 0;
-  //esp_now_send(MAC_RECEIVER, (uint8_t *) &windSpeed, sizeof(windSpeed));   
+  //esp_now_send(MAC_RECEIVER, (uint8_t *) &windSpeed, sizeof(windSpeed));
 
   //Send ESP voltage
   comStruct voltage;
   voltage.datapointID = dpID++;
   strcpy(voltage.id, "vcc");
   strcpy(voltage.sensorType, "ADC_VCC");
-  strcpy(voltage.key, measurands[4]);  
+  strcpy(voltage.key, measurands[4]);
   strcpy(voltage.sValue, "");
   // voltage.dValue = ESP.getVcc();
-  //esp_now_send(MAC_RECEIVER, (uint8_t *) &voltage, sizeof(voltage));   
+  //esp_now_send(MAC_RECEIVER, (uint8_t *) &voltage, sizeof(voltage));
 
 
   //Log to console
@@ -154,7 +156,7 @@ void loop() {
 /**
  *
  */
-ICACHE_RAM_ATTR void rainSensorInterrupt(){  
+ICACHE_RAM_ATTR void rainSensorInterrupt() {
   rainSensorCounter++;
 }
 
@@ -197,8 +199,8 @@ void readBME280(Adafruit_BME280* sens, bme280Struct* vals) {
 }
 
 // Callback when data is sent
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
-  if (sendStatus != 0){    
+void OnDataSent(uint8_t* mac_addr, uint8_t sendStatus) {
+  if (sendStatus != 0) {
     Serial.println("\nLast Packet Send Status: Delivery fail");
   }
 }
